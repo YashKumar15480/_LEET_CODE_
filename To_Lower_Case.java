0070c0634c1581cad92fad835a7f10761fc78426
YASH KUMAR <yaku15480@gmail.com>
2021-07-26 00:56:23 +0530

    LEET CODE

---
+++ To_Lower_Case.java
@@ -0,0 +1,13 @@
+
+class Solution {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
+
+public class To_Lower_Case {
+    public static void main(String[] args) {
+
+    }
+
+}
