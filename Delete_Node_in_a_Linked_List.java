7c356c427e84544246e5c0a193d99b37b8f1142c
YASH KUMAR <yaku15480@gmail.com>
2021-07-26 23:01:02 +0530

    LEET CODE

---
+++ Delete_Node_in_a_Linked_List.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void deleteNode(ListNode node) {
+
+        node.val = node.next.val;
+        node.next = node.next.next;
+
+    }
+}
+
+public class Delete_Node_in_a_Linked_List {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ XOR_Operation_in_an_Array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int xorOperation(int n, int start) {
+        int sum = 0;
+
+        for (int i = 0; i < n; i++)
+            sum ^= (start + 2 * i);
+        return sum;
+    }
+}
+
+public class XOR_Operation_in_an_Array {
+    public static void main(String[] args) {
+
+    }
+
+}
