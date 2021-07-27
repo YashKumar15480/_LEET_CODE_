f6dfd13270d7bc7ccbe65c59d15584f898a6e782
YASH KUMAR <yaku15480@gmail.com>
2021-07-27 10:16:49 +0530

    LEET CODE

---
+++ Power_of_Three.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if (n == 0)
+            return false;
+        if (n == 1)
+            return true;
+
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+        return n == 1;
+
+    }
+}
+
+public class Power_of_Three {
+    public static void main(String[] args) {
+
+    }
+}
