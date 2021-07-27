0c3800cd56fb9e607890546830c5e0d99b6b6848
YASH KUMAR <yaku15480@gmail.com>
2021-07-27 12:25:41 +0530

    LEET CODE

---
+++ Pow(x,n).java
@@ -0,0 +1,13 @@
+class Solution {
+    public double myPow(double x, int n) {
+        return Math.pow(x, n);
+    }
+}
+
+public class pow(x,n)
+{
+
+    public static void main(String[] args) {
+
+    }
+}
