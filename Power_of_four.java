80b73908b30d2bfb971ebe9af8f79f6495a29a5d
YASH KUMAR <yaku15480@gmail.com>
2021-07-27 10:26:45 +0530

    LEET CODE

---
+++ Power_of_four.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        for (int i = 0; i < 31; i++) {
+            {
+                if (Math.pow(4, i) == n)
+                    return true;
+            }
+
+        }
+        return false;
+
+    }
+}
+
+public class Power_of_four {
+    public static void main(String[] args) {
+
+    }
+
+}
