541be91dffac6a99d2de18ecb75792860329c9fd
YASH KUMAR <yaku15480@gmail.com>
2021-07-29 15:03:21 +0530

    LEET CODE

---
+++ Relative_Sort_Array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int[] count = new int[1002];
+        for (int n : arr1)
+            count[n]++;
+        int i = 0;
+        for (int n : arr2) {
+            while (count[n]-- > 0) {
+                arr1[i++] = n;
+            }
+        }
+        for (int n = 0; n < count.length; n++) {
+            while (count[n]-- > 0) {
+                arr1[i++] = n;
+            }
+        }
+        return arr1;
+    }
+}
+
+public class Relative_Sort_Array {
+    public static void main(String[] args) {
+
+    }
+
+}
