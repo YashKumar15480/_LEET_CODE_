f7b110284afdc4060c2fce385f6c43e319d1cf77
YASH KUMAR <yaku15480@gmail.com>
2021-08-01 01:15:21 +0530

    LEET CODE

---
+++ Base_7.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String convertToBase7(int num) {
+        return Integer.toString(num, 7);
+    }
+}
+
+public class Base_7 {
+    public static void main(String[] args) {
+
+    }
+}
