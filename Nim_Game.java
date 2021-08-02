6da16ef6b3f6768cb2f03904e231399978682711
YASH KUMAR <yaku15480@gmail.com>
2021-08-02 11:31:00 +0530

    LEET CODE

---
+++ Nim_Game.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canWinNim(int n) {
+        if (n % 4 == 0)
+            return false;
+        else
+            return true;
+    }
+}
+
+public class Nim_Game {
+    public static void main(String[] args) {
+
+    }
+}
