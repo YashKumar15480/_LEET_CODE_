201844c279349b24cb38dc83002ecc983a1f0863
YASH KUMAR <yaku15480@gmail.com>
2021-08-04 22:43:14 +0530

    LEET CODE

---
+++ Mean_of_Array_After_Removing_Some_Elements.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    public double trimMean(int[] arr) {
+        Arrays.sort(arr);
+        int removeCount = arr.length / 20;
+
+        int sum = 0;
+        for (int i = removeCount; i < arr.length - removeCount; i++) {
+            sum += arr[i];
+        }
+        return sum / (double) (arr.length - 2 * removeCount);
+    }
+}
+
+public class Mean_of_Array_After_Removing_Some_Elements {
+    public static void main(String[] args) {
+
+    }
+
+}
