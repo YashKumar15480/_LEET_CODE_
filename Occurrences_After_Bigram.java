9b939e3e6c16ace07b30af09e18d9d81644e0f59
YASH KUMAR <yaku15480@gmail.com>
2021-08-08 16:14:46 +0530

    LEET CODE

---
+++ Occurrences_After_Bigram.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+
+        if (arr.length == 2 || arr.length == 1 || arr.length == 0)
+            return false;
+
+        for (int i = 0; i < arr.length - 2; i++) {
+            if (arr[i] % 2 != 0 && arr[i + 1] % 2 != 0 && arr[i + 2] % 2 != 0)
+                return true;
+        }
+
+        return false;
+
+    }
+}
+
+public class Occurrences_After_Bigram {
+    public static void main(String[] args) {
+
+    }
+}
