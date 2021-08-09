f4fd345c4186ee00704f535f406109fa1fa6a5ce
YASH KUMAR <yaku15480@gmail.com>
2021-08-09 22:36:18 +0530

    LEET CODE

---
+++ Three_Divisors.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isThree(int n) {
+
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0)
+                count++;
+            if (count == 4)
+                break;
+        }
+
+        if (count == 3)
+            return true;
+        else
+            return false;
+    }
+}
+
+public class Three_Divisors {
+    public static void main(String[] args) {
+
+    }
+}
