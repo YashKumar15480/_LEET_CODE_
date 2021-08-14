570fbc7e1e7b058fa62ce576c14c2b3ddf119fc6
YASH KUMAR <yaku15480@gmail.com>
2021-08-15 00:38:21 +0530

    LEET CODE

---
+++ Valid_Parentheses.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        if (s.length() % 2 != 0)
+            return false;
+
+        Stack st = new Stack<>();
+        HashMap<Character, Character> hp = new HashMap<>();
+
+        hp.put('(', ')');
+        hp.put('{', '}');
+        hp.put('[', ']');
+
+        for (int i = 0; i < s.length(); i++) {
+            if (hp.containsKey(s.charAt(i)))
+                st.push(s.charAt(i));
+            else if (st.empty())
+                return false;
+            else if (hp.get(st.pop()) != s.charAt(i))
+                return false;
+        }
+        if (!st.empty())
+            return false;
+        else
+            return true;
+    }
+}
+
+public class Valid_Parentheses {
+    public static void main(String[] args) {
+
+    }
+}
