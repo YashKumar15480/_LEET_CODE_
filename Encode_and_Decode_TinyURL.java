81682d70a85242a37cce194da73614fa9148cd08
YASH KUMAR <yaku15480@gmail.com>
2021-08-24 00:42:40 +0530

    LEET CODE

---
+++ Encode_and_Decode_TinyURL.java
@@ -0,0 +1,23 @@
+//public 
+class Codec {
+
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        return longUrl;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return shortUrl;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(url));
+
+public class Encode_and_Decode_TinyURL {
+    public static void main(String[] args) {
+
+    }
+}
