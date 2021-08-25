002b5a448b9837a0c393433edb16d7fa8628452e
YASH KUMAR <yaku15480@gmail.com>
2021-08-25 23:54:59 +0530

    LEET CODE

---
+++ Counting_Bits.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] countBits(int n) {
+
+        ArrayList<String> l1 = new ArrayList<>();
+
+        for (int i = 0; i <= n; i++) {
+            String s = Integer.toBinaryString(i);
+            l1.add(s);
+        }
+
+        int a[] = new int[l1.size()];
+
+        for (int i = 0; i < l1.size(); i++) {
+            String s = l1.get(i);
+            int count = 0;
+            for (int j = 0; j < s.length(); j++) {
+                if (s.charAt(j) == '1')
+                    count++;
+            }
+            a[i] = count;
+        }
+
+        return a;
+
+    }
+}
+
+public class Counting_Bits {
+    public static void main(String[] args) {
+
+    }
+}
