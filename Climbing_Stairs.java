9966b7666b652990f387b1383438be08cf6e6fbe
YASH KUMAR <yaku15480@gmail.com>
2021-08-25 22:53:13 +0530

    LEET CODE

---
+++ Climbing_Stairs.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int climbStairs(int n) {
+
+        if (n == 1 || n == 2)
+            return n;
+
+        int a = 1;
+        int b = 2;
+
+        for (int i = 3; i <= n; i++) {
+            b = a + b;
+            a = Math.abs(a - b);
+        }
+        return b;
+
+    }
+}
+
+public class Climbing_Stairs {
+    public static void main(String[] args) {
+
+    }
+
+}
