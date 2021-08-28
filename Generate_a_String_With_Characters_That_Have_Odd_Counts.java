61a235a9c03d218cdea11cd6aa72e9e0066121e6
YASH KUMAR <yaku15480@gmail.com>
2021-08-28 23:14:11 +0530

    LEET CODE

---
+++ Generate_a_String_With_Characters_That_Have_Odd_Counts.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String generateTheString(int n) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n - 1; i++) {
+            sb.append('a');
+        }
+        if (n % 2 == 1) {
+            sb.append('a');
+        } else {
+            sb.append('b');
+        }
+        return sb.toString();
+    }
+}
+
+public class Generate_a_String_With_Characters_That_Have_Odd_Counts {
+    public static void main(String[] args) {
+
+    }
+}
