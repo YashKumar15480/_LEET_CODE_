533e4121f1f42204fad050820f5111f696123b7e
YASH KUMAR <yaku15480@gmail.com>
2021-09-16 23:44:43 +0530

    LEET CODE

---
+++ Reverse_Prefix_of_Word.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String reversePrefix(String word, char find) {
+
+        char ch[] = word.toCharArray();
+
+        int k = 0;
+
+        for (int i = 0; i < word.length(); i++) {
+            if (ch[i] == find) {
+                k = i;
+                break;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = k; i >= 0; i--) {
+            sb.append(ch[i]);
+        }
+
+        for (int i = k + 1; i < word.length(); i++) {
+            sb.append(ch[i]);
+        }
+
+        // System.out.print(k);
+        return sb.toString();
+
+    }
+}
+
+public class Reverse_Prefix_of_Word {
+    public static void main(String[] args) {
+
+    }
+}
