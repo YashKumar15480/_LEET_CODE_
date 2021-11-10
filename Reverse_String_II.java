6238115ca2ad0c4992ab5504c4be135dce46ccd5
YASH KUMAR <yaku15480@gmail.com>
2021-11-10 23:59:01 +0530

    LEET CODE

---
+++ Reverse_String_II.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String reverseStr(String s, int k) {
+        StringBuffer result = new StringBuffer();
+        for (int i = 0; i < s.length() / k + 1; i++) {
+
+            int maxIndex = Math.min(i * k + k, s.length());
+
+            if (i % 2 == 0) {
+                result.append(new StringBuffer(s.substring(i * k, maxIndex)).reverse());
+            } else {
+                result.append(s.substring(i * k, maxIndex));
+            }
+
+        }
+        return result.toString();
+
+    }
+}
+
+public class Reverse_String_II {
+    public static void main(String[] args) {
+
+    }
+
+}
