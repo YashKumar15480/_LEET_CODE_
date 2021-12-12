ba6a8c959e6c2396ad5952d98d1d237731183c77
YASH KUMAR <yaku15480@gmail.com>
2021-12-12 16:06:52 +0530

    LEET CODE

--- Allu.java
+++ Allu.java
@@ -6,28 +6,37 @@ public class Allu {
         int t = sc.nextInt();
         int a = sc.nextInt();
         int b = sc.nextInt();
+        int x = 0, y = 0;
 
         while (t > 0) {
 
             String s = sc.next();
-            int x = 0, y = 0;
+
             for (int i = 0; i < s.length(); i++) {
-                if (s.charAt(i) == 'L')
-                    x--;
-                else if (s.charAt(i) == 'R')
-                    x++;
-                else if (s.charAt(i) == 'D')
-                    y--;
-                else if (s.charAt(i) == 'U')
-                    y++;
+
+                switch (s.charAt(i)) {
+                    case 'L':
+                        x = x - 1;
+                        break;
+                    case 'R':
+                        x = x + 1;
+                        break;
+                    case 'D':
+                        y = y - 1;
+                        break;
+                    case 'U':
+                        y = y + 1;
+                        break;
+                }
 
             }
-            if (a == x || b == y)
-                System.out.println("true");
-            else
-                System.out.println("false");
+
             t--;
         }
+        if (a == x && b == y)
+            System.out.println("true");
+        else
+            System.out.println(x + " -- " + y + "||||" + " -- " + a + " --" + b);
 
     }
 }
